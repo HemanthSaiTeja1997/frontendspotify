746d32378c9cd83d53e313df46f3feaf5d615c66
Hemanth Sai Teja <hemanthsaiteja881@gmail.com>
2022-12-16 08:53:07 +0530

    s

--- src/app/listofsongs/listofsongs.component.ts
+++ src/app/listofsongs/listofsongs.component.ts
@@ -11,6 +11,17 @@ export class ListofsongsComponent {
       alert("Songs Added To Playlist !!!")
       this.route.navigateByUrl("welcome")
   }
-  Listofsongs = [{"url":"/assets/01aAfterTheBall.mp3","name":"Hemanth"},{"url":"/assets/Hunson Abadeer.mp3","name":"Hemanth"} ];
+  Listofsongs = [{"url":"/assets/01aAfterTheBall.mp3","name":"AfterTheBall"},
+  {"url":"/assets/Hunson Abadeer.mp3","name":"HunsonAbadeer"} ,
+  {"url":"/assets/Naruto - I Said Im Naruto.mp3","name":"I Said Im Naruto"},
+  {"url":"/assets/Naruto - Need To Be Strong.mp3","name":"Need To Be Strong"},
+  {"url":"/assets/Naruto - Sadness and Sorrow.mp3","name":"Sadness and Sorrow"},
+  {"url":"/assets/Naruto Shippuden - Naruto and Hinatas love.mp3","name":"Naruto and Hinatas love"},
+  {"url":"/assets/Naruto Shippuden - Opening 1 - Full.mp3","name":"Naruto Shippuden - Opening"},
+  {"url":"/assets/One Piece - Believe.mp3","name":"Believe"},
+  {"url":"/assets/One Piece - Opening 4.mp3","name":"One Piece-Opening"},
+  {"url":"/assets/One Piece - Opening 15.mp3","name":"MinaZukii"},
+
+];
 constructor(private route:Router){}
 }
--- src/app/play/play.component.ts
+++ src/app/play/play.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-play',
   templateUrl: './play.component.html',
   styleUrls: ['./play.component.css']
 })
 export class PlayComponent {
+
 
 }
