a6aad69dc0f71f03e90a07d839d439625a55aa81
Hemanth Sai Teja <hemanthsaiteja881@gmail.com>
2022-12-16 07:58:05 +0530

    save

--- src/app/listofsongs/listofsongs.component.ts
+++ src/app/listofsongs/listofsongs.component.ts
@@ -11,6 +11,6 @@ export class ListofsongsComponent {
       alert("Songs Added To Playlist !!!")
       this.route.navigateByUrl("welcome")
   }
-  Listofsongs: string[] = ["/assets/01aAfterTheBall.mp3", '2.Candle_in_the_Wind', '2.In_the_Summertime', '4.Silent_Nigh', '5.Rock_Around_the_Clock','6.I_Will_Always_Love_You','7.Something_Just_Like_This','8.As_It_Was','9.Calm_Down','10.Anti_Hero'];
+  Listofsongs = [{"url":"/assets/01aAfterTheBall.mp3","name":"Hemanth"},{"url":"/assets/Hunson Abadeer.mp3","name":"Hemanth"} ];
 constructor(private route:Router){}
 }
